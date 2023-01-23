ca0a0b7372ee49974936ae618b3fa914e5d8ca06
etienne-baroth <etienne.baroth@laplateforme.io>
2023-01-23 11:23:36 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+
+<header>
+<div class="head_nav">
+    <div class="head_logo">
+        <img class="logo" src="style/img/logo.png" alt ="logo voyage désert">
+    </div>
+    <div class="head_btn">
+        <p><a href="index.php">Accueil</a></p>
+        <p><a href="connexion.php">Se connecter</a></p>
+        <p><a href="inscription.php">Créer nouveau compte</a></p>
+    </div>
+</div>
+</header>
+
+<main>
+
+<h1>Bienvenue sur le site!</h1>
+
+</main>
+
+</body>
+</html>
